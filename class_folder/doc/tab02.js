8848448bd2309b452d87ca19afa8fd1de57ee035
dugraphy <91935120+dugraphy@users.noreply.github.com>
2021-10-29 11:00:46 +0900

    Add files via upload

---
+++ class_folder/doc/tab02.js
@@ -0,0 +1,15 @@
+$(function(){
+//-----
+//----- this index 사용
+$('.tab_menu>li').on('click', function(){
+    var $this= $(this); //자기자신
+    var idx=$(this).index(); //자기자신의 번호 0,1,2
+    //console.log($this,idx);
+    //$('.tab_content>div').eq(idx).show().siblings().hide();
+    $('.tab_content>div').eq(idx).addClass('on').siblings().removeClass('on');
+    $this.addClass('on').siblings().removeClass('on');
+})
+
+
+//------
+});
