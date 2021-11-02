2d09f0c46ae52f094c7229a2123f60b5fb81e295
dugraphy <91935120+dugraphy@users.noreply.github.com>
2021-11-02 14:00:11 +0900

    Add files via upload

---
+++ class_folder/doc/ytplayer02.js
@@ -0,0 +1,47 @@
+$(function(){
+//-----
+$("#Video").YTPlayer({videoURL:'https://www.youtube.com/embed/Hn1fh7XgXlM',containment:'#visual',
+autoPlay:true, 
+mute:true, 
+startAt:0, 
+opacity:1,
+showControls:false,
+});
+
+$('#visual i.xi-play').on('click', function(){
+    $("#Video").YTPPlay();
+});
+
+$('#visual i.xi-pause').on('click', function(){
+    $("#Video").YTPPause();
+});
+
+$('#visual i.xi-overscan').on('click', function(){
+    $("#Video").YTPFullscreen();
+});
+
+
+$("#mv01").YTPlayer({videoURL:'https://www.youtube.com/embed/Hn1fh7XgXlM',containment:'self',
+autoPlay:true, 
+mute:true, 
+startAt:0, 
+opacity:1,
+showControls:false,
+optimizeDisplay:false,
+});
+
+$('#visual i.xi-play').on('click', function(){
+    $("#Video").YTPPlay();
+});
+
+$('#visual i.xi-pause').on('click', function(){
+    $("#Video").YTPPause();
+});
+
+$('#visual i.xi-overscan').on('click', function(){
+    $("#Video").YTPFullscreen();
+});
+
+
+//------
+});
