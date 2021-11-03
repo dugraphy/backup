7c0703ca505469a73f6009cae3323f28ba5863a8
dugraphy <91935120+dugraphy@users.noreply.github.com>
2021-11-03 14:02:43 +0900

    Add files via upload

---
+++ class_folder/doc/basic05.js
@@ -0,0 +1,16 @@
+$(function(){
+//-----
+
+$('#top_banner i').on('click', function(){
+    $('#top_banner').hide();
+    //--$('#top_banner').slideUp();
+});
+
+$('.main_slider').slick({
+    arrows:false,
+    autoplay:true,
+    autoplaySpeed:2000,
+});
+
+//------
+});
