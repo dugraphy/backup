6205a520cca39889f66b27f4aeabb28b33fb071c
dugraphy <91935120+dugraphy@users.noreply.github.com>
2021-11-04 23:46:26 +0900

    Add files via upload

---
+++ project/01/doc/yanolja_main.js
@@ -0,0 +1,45 @@
+$(function(){
+
+    
+     $(".sub_slide").slick({
+         arrows:false,
+         autoplaySpeed:2000,
+         pauseOnHover:false,
+         infinite: true,
+         slidesToShow: 1,
+         slidesToScroll:1,
+
+
+     });
+
+     $(".sub_slide").eq(0).slick("slickPlay");
+     $("#section03 li").on("click", function(){
+         let idx=$(this).index();
+         $(this).addClass("on").siblings().removeClass("on");
+         $(".sub_slide").eq(idx).addClass("on").siblings().removeClass("on");
+         $(".sub_slide").eq(idx).slick("slickPlay").siblings().slick("slickPause")
+     });
+
+
+
+
+    $('.notice_link').slick({
+         arrows:false,
+         autoplay:true,
+         pauseOnHover:false,
+         vertical:true,
+     });
+    
+     $('#notice i.xi-angle-left-thin').on('click', function(){
+         $('.notice_link').slick('slickPrev');
+     });
+     $('#notice i.xi-angle-right-thin').on('click', function(){
+         $('.notice_link').slick('slickNext');
+     });
+     $('.notice_link').on("afterChange", function(event,slick,current){
+        $("#notice .slider_option span").text( (current+1)+ "/"+slick.slideCount)
+    
+    });
+
+});
+
